f33b7fc1e511235f1cb5668544e3e032fcfc4014
Ishan Sheth <ishan@coffeebeans.io>
2020-12-02 19:22:22 +0530

    adds new settings

--- software_engineer_existential_context_upliftment_and_beyond/gatsby-config.js
+++ software_engineer_existential_context_upliftment_and_beyond/gatsby-config.js
@@ -1,8 +1,20 @@
 module.exports = {
+  pathPrefix: `/software_engineer_existential_context_upliftment_and_beyond`,
   siteMetadata: {
-    name: `Fabian Schultz`,
-    title: `Gatsby Deck`,
-    date: `July 30, 2018`,
+    name: `Ishan Sheth`,
+    date: `December 3, 2020`,
+    title: 'blog.imsheth.com',
+    description: 'cognitive deductions. converged.',
+    itemName: 'Software engineer : existential context, upliftment and beyond',
+    itemDate: 'December 3, 2020',
+    siteUrl: 'https://blog.imsheth.com',
+    itemUrl: 'http://blog.imsheth.com/software_engineer_existential_context_upliftment_and_beyond/',
+    facebook: 'https://www.facebook.com/imsheth',
+    twitter: 'https://twitter.com/theimsheth',
+    linkedIn: 'http://in.linkedin.com/in/imsheth',
+    github: 'https://github.com/imsheth',
+    stackoverflow: 'http://stackoverflow.com/users/3152654/imsheth',
+    instagram: 'https://www.instagram.com/imsheth/',
   },
   plugins: [
     `gatsby-plugin-layout`,
