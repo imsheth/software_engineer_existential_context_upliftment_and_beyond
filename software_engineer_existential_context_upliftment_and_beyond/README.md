703b8e0cb953243626b972762227d8126e5fa162
Ishan Sheth <ishan@coffeebeans.io>
2021-06-08 22:17:59 +0530

    Adds google analytics, removes README.md for avoiding index page load issue

--- software_engineer_existential_context_upliftment_and_beyond/README.md
+++
@@ -1,3 +0,0 @@
-# imsheth/software_engineer_existential_context_upliftment_and_beyond/
-
-The source code for https://imsheth.com/software_engineer_existential_context_upliftment_and_beyond/1, built on https://github.com/fabe/gatsby-starter-deck
--- software_engineer_existential_context_upliftment_and_beyond/gatsby-config.js
+++ software_engineer_existential_context_upliftment_and_beyond/gatsby-config.js
@@ -54,5 +54,11 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-29341891-1`,
+      },
+    },
   ],
 };
